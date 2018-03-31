54b08440acb8f4f1998b2c4c61d68b1c8b8f4874
Daniel Sisse <daniel.cicce@gmail.com>
2018-03-31 17:26:24 +0300

    Тест новой ветки

---
+++ App/View.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+class View
+{
+    protected $data = [];
+
+    public function assign($name, $value)
+    {
+        $this->data[$name] = $value;
+    }
+
+    public function display($template)
+    {
+        include $template;
+    }
+}
