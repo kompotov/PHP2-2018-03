938271327b2859b60b8155f53f65f25ace20ef1d
Daniel Sisse <daniel.cicce@gmail.com>
2018-04-26 19:00:04 +0300

    Поправил шаблоны и класс View

--- App/View.php
+++ App/View.php
@@ -4,8 +4,6 @@ namespace App;
 
 class View
 {
-    protected $count;
-
     use ArbitraryProperties;
 
     /**
@@ -14,5 +12,14 @@ class View
     public function display($template)
     {
         include $template;
+    }
+
+    public function render($template)
+    {
+        ob_start();
+        include $template;
+        $content = ob_get_contents();
+        ob_end_clean();
+        return $content;
     }
 }
